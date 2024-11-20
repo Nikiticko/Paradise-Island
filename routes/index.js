7c13a4c5ca0f8cee1e2804e28603fb83b4f1a6f7
Nikiticko <nikitasemenenko3@gmail.com>
2024-11-21 02:31:37 +0300

    add page about

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Paradise Island' });
+});
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Paradise Island' });
 });
 
 module.exports = router;
