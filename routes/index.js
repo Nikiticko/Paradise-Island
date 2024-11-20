faa637c21bfe0480181b5c325755ec2553bb8508
Nikiticko <nikitasemenenko3@gmail.com>
2024-11-21 02:58:06 +0300

    ready all pages +create folder with fonts

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Paradise Island' });
 });
 /* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Paradise Island' });
+router.get('/menu', function(req, res, next) {
+  res.render('menu', { title: 'Menu' });
 });
-
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+router.get('/galery', function(req, res, next) {
+  res.render('galery', { title: 'Gallery of memories' });
+});
+router.get('/comments', function(req, res, next) {
+  res.render('comments', { title: 'Comments' });
+});
+router.get('/reserv', function(req, res, next) {
+  res.render('reserv', { title: 'Seat reservation' });
+});
 module.exports = router;
